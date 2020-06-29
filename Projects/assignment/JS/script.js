9f7a93b9e47354799e0434afef8e2c073346ef89
Skyler Minsu Bang <32551541+skylermbang@users.noreply.github.com>
2020-06-29 22:18:20 +0900

    Week6

---
+++ Projects/assignment/JS/script.js
@@ -0,0 +1,19 @@
+var phrase = " Skyler is good";
+
+document.write(phrase);
+
+document.write(phrase.length);
+document.write(phrase.toUpperCase());
+document.write(phrase.toLowerCase());
+document.write(phrase.charAt(2));
+
+// skyler is good
+// 0123456789
+// index start from 0
+
+document.write(phrase.indexOf("r"));
+\n;
+document.write(phrase.substring(4, 9));
+\n;
+document.write(phrase.include("skyler"));
+document.write(phrase.endsWith("good"));
