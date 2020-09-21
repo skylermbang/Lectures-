5d4670d6c0c4fa4bdd749614b125edd7bdffa81b
Skyler Minsu Bang <32551541+skylermbang@users.noreply.github.com>
2020-09-22 00:54:18 +0900

    C# 2

---
+++ ps/C#/gradebook/src/GradeBook/Book.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+
+
+namespace GradeBook
+{
+
+    class Book
+    {
+        pubulic void main(double[] grades)
+        {
+
+
+            Console.Write("New grades>>");
+            int New_grade = int.Parse(Console.ReadLine());
+
+            var grades = new List<double>() { 95.1, 90.3, 86.3 };
+            grades.Add(new_grdade);
+        }
+
+    }
+
+
+
+}
