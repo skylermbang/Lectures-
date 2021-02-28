d39155052ddf429ea9ef2ed8f74d997be71970fd
Skyler Minsu Bang <32551541+skylermbang@users.noreply.github.com>
2021-02-28 10:52:43 +0900

    django

--- ps/django/django/meeting_planner/meetings/urls.py
+++ ps/django/django/meeting_planner/meetings/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('<int:id>', views.detail, name="detail"),
     path('rooms/<int:id>', views.roomdetail, name="rooms"),
+    path('new', views.new, name="new"),
 ]
--- ps/django/django/meeting_planner/meetings/views.py
+++ ps/django/django/meeting_planner/meetings/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Meeting
-from .models import Room
+from .models import Meeting, Room
+from django.forms import modelform_factory
+
 
 # Create your views here.
 
@@ -13,3 +14,11 @@ def detail(request, id):
 def roomdetail(request, id):
     room = get_object_or_404(Room, pk=id)
     return render(request, "meetings/roomdetails.html", {"rooms": room})
+
+
+MeetingForm = modelform_factory(Meeting, exclude=[])  # class
+
+
+def new(request):
+    form = MeetingForm()
+    return render(request, "meetings/new.html", {"form": form})
