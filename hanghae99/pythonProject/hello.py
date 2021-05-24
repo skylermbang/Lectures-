0461df1a3d6c2ea23956f0b679f1d6fc3e7e3791
Skyler Minsu Bang <32551541+skylermbang@users.noreply.github.com>
2021-05-24 14:55:05 +0900

    0524

---
+++ hanghae99/pythonProject/hello.py
@@ -0,0 +1,57 @@
+# hello
+
+
+print ("hello sparta")
+
+
+a = 3
+b = 2
+
+print(a*b)
+print (a**b)
+print (a%b)
+
+c = "skyler"
+print (c)
+first_name = "skyler"
+last_name = "bang"
+
+print (first_name + " "+ last_name)
+
+
+g = '2'
+h = str(3)              # also string
+
+print ( g + h)
+
+text = 'abcdefg'
+
+
+
+result = len(text)
+
+result2 = text[:3]  # 0 1 2
+result3 = text[3:]
+result4 = text[3:5]
+print(result)
+print(result2)
+print(result3)
+print(result4)
+
+
+my_email = "skyler@hotmail.com        "
+
+new_result = my_email.split('@')
+
+print(new_result)
+
+
+q1 = "sparta"
+a1 = q1[:3]
+print(a1)
+
+number ="010-9422-0170"
+a2 = number[0:3]
+a3 = number.split("-")
+print (a2)
+print (a3[0])
